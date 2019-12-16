66461be4828e1ebf0b2287a6d95c83e4a379ceea
rvenegas <rvenegas.cv@gmail.com>
2019-12-16 16:29:48 -0300

    uso de playerprefs

---
+++ Assets/Scripts/playerprefs.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerprefs : MonoBehaviour
+{
+    private void Start()
+    {
+
+      //  PlayerPrefs.SetInt("tiempo", 16);
+
+        if (PlayerPrefs.HasKey("tiempo"))
+        {
+            Debug.Log("si existe el tiempo guardado");
+            Debug.Log(PlayerPrefs.GetInt("tiempo"));
+        }
+
+     
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        { 
+        Debug.Log("seteando el tiempo");
+        PlayerPrefs.SetInt("tiempo",15);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            
+            Debug.Log("borrando los datos");
+            //PlayerPrefs.DeleteAll();
+            PlayerPrefs.DeleteKey("tiempo");
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("cargando los datos");
+            //PlayerPrefs.DeleteAll();
+            Debug.Log(PlayerPrefs.GetInt("tiempo"));
+        }
+    }
+}
